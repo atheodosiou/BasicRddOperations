2b7057acc341d5db0f18b8e1b8d9699689c2cda8
atheodosiou <anastasios.theodosiou@gmail.com>
2018-11-06 20:18:14 +0200

    improve word count and sort the results

--- src/main/scala/FakeFriends.scala
+++ src/main/scala/FakeFriends.scala
@@ -1,5 +1,76 @@
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.log4j.{Level, Logger}
+
 object FakeFriends {
   def main(args: Array[String]): Unit = {
-    println
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    println("Calculate avarage count of friends per age using Spark!")
+
+    val conf = new SparkConf()
+      .setMaster("local[*]")
+      .setAppName("AvarageFriendsCount")
+    val sc = new SparkContext(conf)
+
+    //This function prepares the dataset
+
+    def parseLineForAge(line: String)={
+      val fields = line.split(",")
+      val age = fields(1).toInt
+      val numOfFriends = fields(2).toInt
+
+      (age,numOfFriends)
+    }
+
+    def parseLineForNames(line: String)={
+      val fields = line.split(",")
+      val firstname = fields(0)
+      val numOfFriends = fields(2).toInt
+
+      (firstname,numOfFriends)
+    }
+
+    //Load our dataset from local file system
+
+    val lines = sc.textFile("src/Data/data.csv")
+
+    //Foreach line of dataset run parseLine to parse data
+    val rdd = lines.map(parseLineForAge)
+    val rdd2 = lines.map(parseLineForNames)
+
+    println("Initial dataset by age:")
+    //(27,789)
+    println("")
+    rdd.foreach(println)
+    println("")
+
+    println("Initial dataset by firstname:")
+    //(tasos,1456)
+    println("")
+    rdd2.foreach(println)
+    println("")
+
+    val totals=rdd.mapValues(x=>(x,1)).reduceByKey((x,y)=>(x._1+y._1,x._2+y._2))
+    val totals2=rdd2.mapValues(x=>(x,1)).reduceByKey((x,y)=>(x._1+y._1,x._2+y._2))
+
+    println("Total counts by age:")
+    println("")
+    totals.foreach(println)
+    println("")
+    println("Avarege friends per age:")
+    println("")
+
+    val averageFriendsByAge= totals.mapValues(x=> (x._1 / x._2))
+    val averageFriendsByFirstname= totals2.mapValues(x=> (x._1 / x._2))
+
+    val results= averageFriendsByAge.collect()
+    results.sorted.foreach(println)
+
+    println("Avarege friends per firsname:")
+    println("")
+
+    val results2= averageFriendsByFirstname.collect()
+    results2.sorted.foreach(println)
   }
 }
--- src/main/scala/WordCount.scala
+++ src/main/scala/WordCount.scala
@@ -1,3 +1,57 @@
-object WordCount {
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.log4j.{Level, Logger}
 
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    println("Word Count using flatMap")
+
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    //Create Spark Context
+    val sc = new SparkContext("local[*]","MinTemperatures")
+
+    //Load data
+    val lines = sc.textFile("src/Data/book.txt")
+
+    // split the hole document into words
+//    val words = lines.flatMap(x => x.split(" "))
+
+    //Split using a requar expression that extracts words
+    val words = lines.flatMap(x => x.split("\\W+"))
+
+    //Normalize everything to lowercase
+    val lowercaseWords = words.map(x=>x.toLowerCase())
+
+//    println("Split the hole document into words")
+//    println("Sample from 10 first words:")
+//    println("")
+//    lowercaseWords.take(10).foreach(println)
+//    println("")
+//
+//    //Count up the occurrences of each word
+//    println("Count up the occurrences of each word")
+//    val wordCounts = lowercaseWords.countByValue();
+//    println("")
+//
+//    println("Print results:")
+//    wordCounts.foreach(println)
+//    println("")
+//
+//    println("Get most frequent words ( > 500 ):")
+//    println("")
+//    val frequent= wordCounts.filter(x=>x._2 >= 500)
+//    frequent.foreach(println)
+
+//    count of the occurrences of each word
+    val wordCounts = lowercaseWords.map(x=>(x,1)).reduceByKey((x,y) => x+y)
+
+//    Flip (word, count) tuples to (count, word ) and then short by key (the counts)
+    val wordCountSorted = wordCounts.map(x=>(x._2,x._1)).sortByKey()
+    for(result <- wordCountSorted){
+      val count = result._1
+      val word = result._2
+      println(s"$word: $count")
+    }
+
+  }
 }
